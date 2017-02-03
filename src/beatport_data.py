9f0cad1c4a931e95748421f46840b2e7b0382e48
Danius Silkaitis <danius.silkaitis@gmail.com>
2017-02-03 14:14:37 -0800

    Script to (re)build Beatport data tables

---
+++ src/beatport_data.py
@@ -0,0 +1,23 @@
+import psycopg2 as pg2
+
+from beatport_api import beatport, sqlport
+
+def build_artist_table(btpt):
+    '''
+    Build artist table
+    '''
+    artists = btpt.find_all_artists_by_genre_id(1)
+
+    slpt = sqlport()
+    slpt.build_artist_table(artists)
+    return
+
+
+if __name__ == '__main__':
+    '''
+    Build or rebuild Beatport SQL tables
+    '''
+    btpt = beatport('~/beatnet/mykeys.yaml')
+    btpt.initialize()
+
+    build_artist_table(btpt)
