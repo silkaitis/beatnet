be62b364d701a9030449c05a4bee3ddca191a71e
Danius Silkaitis <danius.silkaitis@gmail.com>
2017-02-14 14:05:51 -0800

    Collect all Beatport IDs for DnB

---
+++ src/track_id_set.py
@@ -0,0 +1,24 @@
+import cPickle as pkl
+from beatport_api import beatport
+
+if __name__ == '__main__':
+
+    bprt = beatport('/Users/danius/galvanize/API/mykeys.yaml')
+    bprt.initialize()
+
+    yrs = range(1990,2017)
+
+    trk_ids = set()
+    for yr in yrs:
+        print 'Processing {}'.format(yr)
+
+        start = str(yr) + '-01-01'
+        stop = str(yr) + '-12-31'
+
+        result = bprt.tracks_w_dates(start, stop)
+
+        for r in result:
+            trk_ids.add(r['id'])
+
+    with open('track_id_set.pkl', 'w') as fin:
+        pkl.dump(trk_ids, fin)
