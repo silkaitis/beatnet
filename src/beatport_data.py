573deb7db9bf37b1c23ee6f269cc5e62338b8a03
Danius Silkaitis <danius.silkaitis@gmail.com>
2017-02-15 13:23:29 -0800

    Reduce memory

--- src/beatport_data.py
+++ src/beatport_data.py
@@ -17,34 +17,6 @@ def build_artist_table(bprt, name):
     slpt.build_artist_table(artists)
     return
 
-def _setup_progress_bar(num_pages):
-    curses.initscr()
-    curses.curs_set(0)
-
-    sentence = 'Page {} of {} complete.'.format(' '*len(str(num_pages)), str(num_pages))
-
-    sys.stdout.write(sentence)
-    sys.stdout.flush()
-
-    sys.stdout.write('\b' * (len(sentence) - 5))
-
-    return
-
-def _update_progress_bar(curr_page):
-    pg = str(curr_page)
-
-    sys.stdout.write(pg)
-    sys.stdout.flush()
-    sys.stdout.write('\b' * len(pg))
-
-    return
-
-def _escape_progress_bar():
-    sys.stdout.write('\n')
-    curses.curs_set(1)
-    curses.reset_shell_mode()
-    return
-
 if __name__ == '__main__':
     now = time()
     '''
@@ -64,15 +36,11 @@ if __name__ == '__main__':
     with open('../data/track_id_set.pkl', 'r') as fin:
         trk_id_set = pkl.load(fin)
 
-    essentia = essentia_api()
-
     trk_id_set = list(trk_id_set)
-
-    _setup_progress_bar(len(trk_id_set))
 
     for i, trk in enumerate(trk_id_set):
 
-        _update_progress_bar(i)
+        essentia = essentia_api()
 
         fname = bprt.save_track_snippet(trk_id_set[i], '../samples/')
 
@@ -80,9 +48,9 @@ if __name__ == '__main__':
 
         os.remove(fname)
 
-        sleep(random.sample([0.5,1,1.5,2], 1)[0])
+        del essentia
 
-    _escape_progress_bar()
+        sleep(random.sample([0.5, 1, 1.5], 1)[0])
 
     t = (time() - now) * (1/60.)
 
