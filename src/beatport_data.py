77c5fcc154639e8647964c853151c6adcb316652
Danius Silkaitis <danius.silkaitis@gmail.com>
2017-02-15 12:47:36 -0800

    Full Library Feature Extraction

--- src/beatport_data.py
+++ src/beatport_data.py
@@ -5,7 +5,7 @@ import psycopg2 as pg2
 
 from beatport_api import beatport, sqlport
 from essentia_api import essentia_api
-from time import time
+from time import time, sleep
 
 def build_artist_table(bprt, name):
     '''
@@ -15,6 +15,34 @@ def build_artist_table(bprt, name):
 
     slpt = sqlport(name)
     slpt.build_artist_table(artists)
+    return
+
+def _setup_progress_bar(num_pages):
+    curses.initscr()
+    curses.curs_set(0)
+
+    sentence = 'Page {} of {} complete.'.format(' '*len(str(num_pages)), str(num_pages))
+
+    sys.stdout.write(sentence)
+    sys.stdout.flush()
+
+    sys.stdout.write('\b' * (len(sentence) - 5))
+
+    return
+
+def _update_progress_bar(curr_page):
+    pg = str(curr_page)
+
+    sys.stdout.write(pg)
+    sys.stdout.flush()
+    sys.stdout.write('\b' * len(pg))
+
+    return
+
+def _escape_progress_bar():
+    sys.stdout.write('\n')
+    curses.curs_set(1)
+    curses.reset_shell_mode()
     return
 
 if __name__ == '__main__':
@@ -36,19 +64,25 @@ if __name__ == '__main__':
     with open('../data/track_id_set.pkl', 'r') as fin:
         trk_id_set = pkl.load(fin)
 
-    essentia = essentia_api('audio_summary.json')
+    essentia = essentia_api()
 
     trk_id_set = list(trk_id_set)
-    i = 0
-    while i < 10:
-        print('Processing track id: {}'.format(trk_id_set[i]))
+
+    _setup_progress_bar(len(trk_id_set))
+
+    for i, trk in enumerate(trk_id_set):
+
+        _update_progress_bar(i)
+
         fname = bprt.save_track_snippet(trk_id_set[i], '../samples/')
 
         essentia.execute(fname)
 
         os.remove(fname)
 
-        i += 1
+        sleep(random.sample([0.5,1,1.5,2], 1)[0])
+
+    _escape_progress_bar()
 
     t = (time() - now) * (1/60.)
 
